3d2b1506a9d1a6d00437bbe7e0085b489ede0f93
Kamil <kamil.zachara98@gmail.com>
2022-01-26 19:18:17 +0100

    Srv dev started, database connected, "works" kinda'

---
+++ src/main/java/com/example/demo/contact/ContactController.java
@@ -0,0 +1,34 @@
+package com.example.demo.contact;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/contact")
+public class ContactController {
+
+//    ArrayList<Contact>contacts=new ArrayList<>();//irl tu wpadają rzeczy z serwera
+//        contacts.add(new Contact("Maddy Perez","madper@wp.pl","https://static.wikia.nocookie.net/euphoria-hbo/images/1/13/Maddy_Perez.jpg/revision/latest?cb=20200821140939.jpg"));
+//        contacts.add(new Contact("Jules Vaughm","juulz@wp.pl","https://static.wikia.nocookie.net/euphoria-hbo/images/0/06/Jules_Vaughn.jpg/revision/latest/scale-to-width-down/1200?cb=20190617063942.jpg"));
+//        contacts.add(new Contact("cassie Howard","cascas@wp.pl","https://static.wikia.nocookie.net/euphoria-hbo/images/f/f9/Euphoria_S2_Cassie.jpg/revision/latest?cb=20220104035238.jpg"));
+//        contacts.add(new Contact("Elliot","ellie@wp.pl","https://static.wikia.nocookie.net/euphoria-hbo/images/7/7d/Euphoria_S2_Elliot.jpg/revision/latest?cb=20211225055956.jpg"));
+
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @GetMapping
+    public List<Contact> getContacts(){
+        return contactService.getContacts();
+    }
+
+}
+
---
+++ src/main/java/com/example/demo/contact/ContactService.java
@@ -0,0 +1,25 @@
+package com.example.demo.contact;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContactService {
+
+    private final ContactRepository contactRepository;
+
+    @Autowired
+    public ContactService(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+
+    public List<Contact> getContacts() {
+
+        return contactRepository.findAll();
+
+    }
+}
